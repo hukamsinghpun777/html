427abc8eda8204aaf170a81aece69866f7ebedb5
Hukam Singh <hukamsinghpun777@gmail.com>
2024-01-17 13:35:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// let hello = "hello java";
+// let world = "my first java code";
+
+// console.log(hello)
+// console.log(world)
+
+
+
+// function hello(a,b,c){
+//     console.log(a+b*c)
+// }
+// hello(99,68,45)
+
+function apple(){
+    console.log(2)
+}
+apple();
